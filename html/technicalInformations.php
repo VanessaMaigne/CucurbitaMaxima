0cecdca5c4fce7f1c751667c22f71da0d277e5e4
VanessaMaigne <vmaigne@gmail.com>
2015-08-25 12:08:27 +0200

    Technical informations

---
+++ html/technicalInformations.php
@@ -0,0 +1,36 @@
+<HTML>
+
+<HEAD>
+    <meta http-equiv="Content-Type" content="text/html;charset=utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>Cucurbita Maxima Technical Informations</title>
+    <link rel="icon" href="../img/tomato_16.png" type="image/png">
+
+    <!-- ************************* CSS ************************* -->
+    <link rel="stylesheet" type="text/css" href="../js/bootstrap-3.1.1-dist/css/bootstrap.min.css">
+    <link rel="stylesheet" type="text/css" href="../css/home.css">
+
+    <!-- ************************* JS ************************* -->
+</HEAD>
+
+
+<BODY>
+
+<div class="container containerHome">
+    <div>
+        <div class="col-sm-2 col-md-2"><BR/>
+            <a href="../index.html"><img src="../img/120.png" class="basicButton" width="50px" title="Retour page d'accueil"/></a>
+        </div>
+
+        <div class="col-sm-8 col-md-10 text-center"><h2 class="technicalTitle">Données techniques</h2><BR/><BR/><BR/></div>
+    </div>
+
+    <div class="technicalSubTitle">Classification APG III (2009)</div>
+    <BR/><img src="../img/classification_APGIII.PNG"/><BR/><BR/>
+
+</div>
+
+</BODY>
+</HTML>
+
